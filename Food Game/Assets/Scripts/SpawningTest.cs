d3cf4dc3f9d031a670a36f1f02fdb5f2954e4668
andy-thipp <56776825+andy-thipp@users.noreply.github.com>
2020-02-09 19:15:19 -0500

    Spawn testing
    Made a simple over time spawn point; added player, enemy, and spawn point prefabs

---
+++ Food Game/Assets/Scripts/SpawningTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningTest : MonoBehaviour
+{
+    // Reference the prefab
+    public GameObject testEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            GameObject.Instantiate(testEnemy, transform.position, Quaternion.identity);
+            // Spawn every x amount of seconds
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
