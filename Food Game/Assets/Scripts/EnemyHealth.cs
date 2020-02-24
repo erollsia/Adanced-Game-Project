18c2b8db83fd3d75378fb0b57c05dddc0f580b65
ebotkin12 <ethan.man98@gmail.com>
2020-02-24 13:49:29 -0500

    Added basic enemy health functionality

---
+++ Food Game/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxEHealth = 50;
+    public int pDamage = 10;
+    int eHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eHealth = maxEHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("l"))
+        {
+            eHealth -= pDamage;
+        }
+
+        if (eHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void onTrigerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "attack")
+        {
+            eHealth -= pDamage;
+        }
+    }
+}
