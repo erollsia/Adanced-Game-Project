3187bbab660f8b5327637ff4acfa7dbe415845ba
andy-thipp <56776825+andy-thipp@users.noreply.github.com>
2020-02-24 19:21:41 -0500

    Beginning player attacks
    Started on player melee and ranged attacks; added testing ranged weapon

---
+++ Food Game/Assets/Scripts/playerRangedAttack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerRangedAttack : MonoBehaviour
+{
+    public float offset;
+
+    public GameObject projectile;
+    public Transform shotPoint;
+
+    private float timedShots;
+    public float startTimeShots;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Weapon rotation
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+        //Projectile location
+        if(timedShots <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                timedShots = startTimeShots;
+            }
+        }
+        else
+        {
+            timedShots -= Time.deltaTime;
+        }
+    }
+}
---
+++ Food Game/Assets/Scripts/projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+
+    // Need on hit effect for projectile for below
+    public GameObject hitEffect;
+
+    private void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+
+    private void Update()
+    {
+        transform.Translate(transform.up * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile()
+    {
+        Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
