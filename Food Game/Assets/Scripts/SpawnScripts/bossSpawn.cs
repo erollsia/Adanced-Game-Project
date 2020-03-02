6efdbb6d1038162b439f79bbc1d42c84d9b94bf1
andy-thipp <56776825+andy-thipp@users.noreply.github.com>
2020-03-01 20:42:04 -0500

    Boss Spawn and item spawn start
    Begun boss spawn; item spawn

---
+++ Food Game/Assets/Scripts/SpawnScripts/bossSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossSpawn : MonoBehaviour
+{
+    public GameObject bossEnemy; // Enemy reference
+    public float sGoal, mGoal, lGoal = 0; // How many of each type to defeat
+    private static int sDefeat, mDefeat, lDefeat = 0; // Counter for each
+    public float enemyLimit = 0; // Enemy limiter
+    private bool objectiveComplete = false;
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        if (sDefeat == sGoal & mDefeat == mGoal & lDefeat == lGoal)
+        {
+            objectiveComplete = true;
+        }
+
+        while (objectiveComplete = true)
+        {
+            GameObject.Instantiate(bossEnemy, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(5.0f); // Spawn enemy every x seconds
+            objectiveComplete = false;
+        }
+    }
+}
